c6884e7f20cd1c2ed257091d6c49728f9b42b67a
U-BSD\331977 <331977@SHS-10D43CBCB.beaverton.k12.or.us>
2017-09-25 11:27:56 -0700

    first commit

---
+++ palindrone.cpp
@@ -0,0 +1,55 @@
+//Program made by Jermaine Lara
+//9/25/17
+//Program compares user input with its backwards counterpart to see if it's a palindrone.
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+// can use <cstring> strcmp strlen and strcpy can't use strings!
+
+int main()
+{
+  
+  char input[81];
+  char cleanInput[81];
+  char backwardsInput[81];
+
+  //Get user input
+  cin.get(input,81);
+
+  //Remove punctuation and whitespaces
+  int j=0;
+  for(int i=0; i<81; i++) {
+    if(input[i] !=' ' && input[i] !=',' && input[i] !='!'
+       && input[i] !=':' && input[i] !=';' && input[i] !='?'
+       && input[i] !='"') {
+      cleanInput[j] = input[i];
+      j++;
+    }
+  }
+
+  //Convert to lowercase
+  int length = strlen(cleanInput);
+  for (int i =0; i< length; i++) {
+    if(cleanInput[i] >='A' && cleanInput[i] <= 'Z') {
+      cleanInput[i] +=32;
+    }
+  }
+
+  //Create a backwards input array
+  for(int i=0; i<length; i++) {
+    backwardsInput[length - i -1] = cleanInput[i];
+  }
+  backwardsInput[length] = 0;
+  
+  //Compare the clean input and the backwards input
+  if(strcmp(cleanInput,backwardsInput) == 0) {
+  cout <<"Palindrone." << endl;
+} else {
+  cout <<"Not a Palindrone." << endl;
+ }
+      
+  
+}
+# Palindrone
